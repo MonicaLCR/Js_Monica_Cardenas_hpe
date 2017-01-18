3744964aa3858e0ea5391d70c6be537896ab5dbe
MonicaLCR <monique.cardenas2102@gmail.com>
2017-01-18 11:46:53 -0600

    Add files via upload

---
+++ hpe_training_js_session1_monica-cardenas.js
@@ -0,0 +1,39 @@
+//3.- Declare 5 different variables, it can be either a number or a String
+var myName='monica';
+var age = 27;
+var career;
+var yearExp = 'i have 4 years of experience';
+var hob;
+
+//5.- Declare at least 5 arrays, either only numbers, Strings or mixed.*/
+var dias = ["lunes", "martes", "miercoles", "jueves", "viernes"];
+var paises = ["Mexico", "Brasil", "Estados Unidos", "Alemania"];
+var numeros = ["uno", "dos","tres","cuatro"];
+var nombres = ["zutanito", "perenganito", "fulanito"];
+var puntos = [100, 200,300,400, 500];
+
+//2.- Review the length of at least 5 words/sentences.
+console.log(dias.length);
+console.log(myName.length);
+console.log(puntos.length);
+console.log(yearsExp.length);
+console.log(nombres.length);
+
+
+//1.- Write at least 10 lines of js code that prints out a phrase, it can be whatever you wanna print.
+console.log("hello");
+console.log("my name is monica");
+console.log("i am 27 years old");
+console.log("i am computer engineer");
+console.log("i have 4 years of experience");
+console.log("i want to learn more about javascript");
+console.log("i like to sing");
+console.log("i like to hear rock music");
+
+
+//4.- Ask a user 5 different questions.
+var userChoice = prompt("what is your name?");
+var userChoice = prompt("how old are you?");
+var userChoice = prompt("what did you study");
+var userChoice = prompt("do you believe in you?")
+var userChoice = prompt("do you belive in people?");
